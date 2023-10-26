8a0d7af0198883f4c32959e3d8544740be8fabe8
jeovabastos <jbastos.im@gmail.com>
2023-10-25 21:24:21 -0400

    First the content

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Jeremyah Sculptures Studio</h1>
+      <div>
+        <img src='/dante.jpeg'/>
+        <img src='/dante-1.jpeg'/>
+        <img src='/dante-2.jpeg'/>
+        <img src='/dante-3.jpeg'/>
+        <img src='/dante-4.jpeg'/>
+        <img src='/dante-5.jpeg'/>
+        <img src='/dante-6.jpeg'/>
+        <img src='/dante-7.jpeg'/>
+        <img src='/dante-8.jpeg'/>
+      </div>
+    </>
+  )
+}
+
+export default App
